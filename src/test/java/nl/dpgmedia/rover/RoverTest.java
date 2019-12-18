99a52a5104f555a3823b883d6680eadd7d286da5
Doménique Tilleuil <domenique@tripled.io>
2019-12-18 20:07:28 +0100

    initial commit

---
+++ src/test/java/nl/dpgmedia/rover/RoverTest.java
@@ -0,0 +1,12 @@
+package nl.dpgmedia.rover;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RoverTest {
+
+    @Test
+    void runsTests() {
+        Assertions.assertTrue(true);
+    }
+}
